4e9c302538aa3ebd338bf44b429e1c912a2fcfd8
Danicadel29 <danica.delfosse@student.heaj.be>
2025-05-19 11:32:21 +0200

    ajout du cursor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,3 +5,36 @@
   burger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
   });
+
+
+  //  Curseur personnalisé //
+const body = document.body;
+const Cursor = document.querySelector('.cursor');
+const images = document.querySelectorAll('.cursor-img');
+
+//deplacer le cursor et centre
+function moveCursor(e) {
+    console.log('mousemove');
+  Cursor.style.top = `${(e.clientY + window.scrollY) - 32}px`; 
+  Cursor.style.left = `${e.clientX - 32}px`;
+}
+// cacher le cursor de base / afficher le cursor personnaliser/ activer le suivi
+    images.forEach((img) => {
+        img.addEventListener('mouseenter', () => {
+            console.log('mouseenter');
+        Cursor.style.visibility = 'visible';
+        body.style.cursor = 'none';
+        img.addEventListener('mousemove', moveCursor);
+        });
+    });
+//inverse que on part de l'element avec le cursor
+    images.forEach((img) => {
+        img.addEventListener('mouseleave', (e) => {
+            console.log('mouseleave');
+        if (!img.contains(e.relatedTarget)) {
+            Cursor.style.visibility = 'hidden';
+            body.style.cursor = 'default';
+            img.removeEventListener('mousemove', moveCursor);
+        }
+    });
+});
